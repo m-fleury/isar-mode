a3afc3e107340e81c8e6060702869c71bf0cc96c
Mathias Fleury <Mathias.Fleury@ens-rennes.fr>
2020-01-06 13:14:18 +0100

    add Cask file

--- isar-mode.el
+++ isar-mode.el
@@ -1,3 +1,36 @@
+;;; isar-mode.el --- Simple Isar Mode -*- lexical-binding: t -*-
+
+;; Copyright (C) 2018-2020 Mathias Fleury
+;; URL: https://bitbucket.org/zmaths/isabelle2019-vsce/
+
+;; Keywords: lisp
+;; Version: 0
+;; Package-Requires: ((emacs "25.1") (lsp-mode "6.0"))
+
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and-or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+
+;; The above copyright notice and this permission notice shall be included in
+;; all copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+
+;;; Commentary:
+
+;; blabla
+
+;;; Code:
+
 (require 'isar-unicode-tokens)
 (require 'unicode-tokens)
 
@@ -388,3 +421,5 @@
 (setq spacemacs-jump-handlers-isar-mode nil)
 
 (provide 'isar-mode)
+
+;;; isar-mode.el ends here
